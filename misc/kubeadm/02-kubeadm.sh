e6d8edaca3bfa172406d1f3909d58cb6403cfcf0
Rajesh G <rajesh@unigps.in>
2022-06-17 02:15:48 +0530

    refine

--- misc/kubeadm/02-kubeadm.sh
+++ misc/kubeadm/02-kubeadm.sh
@@ -15,6 +15,11 @@ sudo systemctl enable kubelet
 
 IPADDR=$(wget -qO-  http://checkip.amazonaws.com)
 
+rm /etc/containerd/config.toml
+systemctl restart containerd
+sudo kubeadm init  --pod-network-cidr=192.168.0.0/16  
+sudo systemctl start kubelet
+
 #sudo kubeadm init --apiserver-advertise-address=$IPADDR --pod-network-cidr=192.168.0.0/16  --cri-socket /run/containerd/containerd.sock  
 #sudo kubeadm init  --pod-network-cidr=192.168.0.0/16  
 
